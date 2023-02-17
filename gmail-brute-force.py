09e33bef6e280c6970479903ac4761bc9d94a8b4
kawedi <oag1356@rit.edu>
2023-02-16 22:36:23 -0500

    smtp and user input

--- gmail-brute-force.py
+++ gmail-brute-force.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
-import argparse
-
+import argparse, smtplib
 
 #
 # Connect to GMail's SMTP server 
 #
 
 def smtp():
-    print()
-
-#
-# help output 
-#
-
-def help():
-    print()
-
+    server = smtplib.SMTP("smtp.gmail.com", 200)
+    server.ehlo()
+    server.starttls()
+    return server
 
 def cmd_parse(): 
     # Create the parser and add arguments
     parser = argparse.ArgumentParser(prog="gmail-brute-force.py", 
              description="Brute-force GMail accounts",
              epilog="Made for educational purposes. Creator: Olivia Gallucci")
-    parser.add_argument("target_email", help="email you want to attack")
-    parser.add_argument("password_list", help="/path/password_list.txt")
-    parser.add_argument("attack_duration", help="amount of seconds you want to wait between attacks")
-    # parser.add_argument('--c', dest='--email', help="Target email")
-    # parser.add_argument(dest='--plist', help="Password list")
+    parser.add_argument("target_email", type=str, help="email you want to attack")
+    parser.add_argument("password_list", type=str, help="[/path/password_list.txt] Each password should be on a new line")
+    parser.add_argument("attack_duration", type=int, help="amount of seconds you want to wait between attacks")
 
-
-    # Parse and print the results
     args = parser.parse_args()
-    print(args.argument1)
+    return args
+
+    
 
 
 def main():
     # user initial input 
-    cmd_parse()
+    args = cmd_parse()
+
+    print(args)
+
+    # make a list of the passwords from a file 
+    with open(args.password_list) as file:
+        pass_list = file.readlines()
+        
+    # remove new line characters
+    pass_list = [x.strip() for x in pass_list]
+    print(pass_list)
+
+    file.close()
+
+
+    # Parse and print the results
+    # args = parser.parse_args()
+    # print(args.argument1)
+
+
     user_input = 'q' # input("prompt")
 
 
